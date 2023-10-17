4916803da8a635088e6dc78daea445eafb238bbe
haticesaike <haticesaike374@gmail.com>
2023-10-18 01:14:21 +0300

    refactor

--- day3-RotatingNavigation/script.js
+++ day3-RotatingNavigation/script.js
@@ -5,16 +5,18 @@ const nav = document.getElementById('navigationSide');
 
 
 function openClickButton(){
-    open.style.display = 'none';
+    open.style.transform = 'translateY(-100%)';
     close.style.transform = 'translateX(0)';
     close.style.transform= 'rotate(20deg)';
     open.style.transition = 'all 1.2s ease-in-out';
     container.classList.add('show-nav');
     nav.style.display = 'block';
+    nav.style.transform = 'translateX(0)';
 }
 
 function closeClickButton(){
     open.style.display = 'block';
+    open.style.transform = 'translateX(0)';
     close.style.transform= 'rotate(90deg)';
     container.classList.remove('show-nav');
     nav.style.display = 'none';
